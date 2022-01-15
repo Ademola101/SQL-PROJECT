e574f189401fd12711826c8ab7753a50977cbb29
Ademola <ademolaogunmokun@yahoo.com>
2022-01-15 16:30:55 +0100

    answers 75

--- answers.sql
+++ answers.sql
@@ -19,3 +19,12 @@ SELECT name FROM world
 WHERE population >= 200000000;
 
 -- 6 Per capita
+
+SELECT name, GDP/population as Per_Capita
+  FROM world
+ WHERE population >= 200000000;
+
+-- 7 Population of South America continent in millions
+SELECT name, population/1000000 as Population_in_million
+FROM world
+WHERE continent = 'South America';
